c245503d05e5022f138fda029596a3182aab7792
Rodrigo Porto Schwedersky <rodrigo_schwedersky@estudante.sesisenai.org.br>
2022-03-08 10:56:06 -0300

    service cogum

--- src/services/cogums.js
+++ src/services/cogums.js
@@ -14,37 +14,49 @@ import shimofuriEspecie from './../assets/especies/shimofuriEspecie.webp'
       name: 'Pleurotus Ostreatus',
       comName:'Shimeji branco',
       image: brancoEspecie,
-      estacao: ['verao','inverno']
+      estacao: ['verao','inverno'],
+      descricao:'O Shimeji branco (Pleurotus ostreatus ) é uma fonte rica em proteínas, vitaminas, minerais, fibras e outros antioxidantes como o selênio, protegendo as células do corpo de danos que podem levar a doenças crônicas e ajudam a fortalecer o sistema imunológico. Possui baixas calorias, sem gordura, sem colesterol, sem glúten e com baixo teor de sódio. O consumo contínuo de Shimeji parece diminuir o risco de obesidade, diabetes, câncer, doenças cardíacas e fortalecer o sistema imune do corpo.',
+      referência: 'https://mushroomreferences.com'
     },
     {
       name: 'Pleurotus Citrinopilleatus',
       comName:'Shimeji amarelo',
       image: amareloEspecie,
-      estacao: ['verao','inverno']
+      estacao: ['verao','inverno'],
+      descricao:'O Shimeji amarelo (Pleurotus Citrinopileatus) é uma fonte rica em proteínas, vitaminas, minerais, fibras e outros antioxidantes como o selênio, protegendo as células do corpo de danos que podem levar a doenças crônicas e ajudam a fortalecer o sistema imunológico. Possui baixas calorias, sem gordura, sem colesterol, sem glúten e com baixo teor de sódio. O consumo contínuo de Shimeji parece diminuir o risco de obesidade, diabetes, câncer, doenças cardíacas e fortalecer o sistema imune do corpo.',
+      referência: 'https://mushroomreferences.com'
     },
     {
       name: 'Hericium erinaceus',
       comName:'Juba de Leão',
       image: lionsEspecie,
-      estacao: ['verao','inverno']
+      estacao: ['verao','inverno'],
+      descricao:'O cogumelo Lions Mane (Hericium erinaceus) também é conhecido como hou tou gu ou yamabushitake e eles receberam seu nome moderno pela aparência à medida que cresce. É um cogumelo branco, grande e desgrenhado que se assemelha a uma juba de leão branco. Doenças neurodegenerativas como Alzheimer e demência são condições que resultam da degeneração celular no cérebro. A capacidade do nosso cérebro de conectar novas conexões diminui com o tempo e essa é uma possível explicação para o fato de que, quando envelhecemos, tendemos a enfraquecer as funções do nosso cérebro. Estudos descobriram que existem dois compostos especiais nos cogumelos juba de leão que podem estimular o crescimento das células cerebrais: hericenonas e erinacinas.',
+      referência: 'https://mushroomreferences.com'
     },
     {
       name: 'Pleurotus Ostreatus',
       comName:'Shimeji pérola',
       image: perolaEspecie,
-      estacao: ['verao','inverno']
+      estacao: ['verao','inverno'],
+      descricao:'O Shimeji branco (Pleurotus ostreatus ) é uma fonte rica em proteínas, vitaminas, minerais, fibras e outros antioxidantes como o selênio, protegendo as células do corpo de danos que podem levar a doenças crônicas e ajudam a fortalecer o sistema imunológico. Possui baixas calorias, sem gordura, sem colesterol, sem glúten e com baixo teor de sódio. O consumo contínuo de Shimeji parece diminuir o risco de obesidade, diabetes, câncer, doenças cardíacas e fortalecer o sistema imune do corpo.',
+      referência: 'https://mushroomreferences.com'
     },
     {
       name: 'Pleurotus Djamor',
       comName:'Shimeji rosa',
       image: rosaEspecie,
-      estacao: ['verao','inverno']
+      estacao: ['verao','inverno'],
+      descricao:'O Shimeji rosa (Pleurotus djamor ) é uma fonte rica em proteínas, vitaminas, minerais, fibras e outros antioxidantes como o selênio, protegendo as células do corpo de danos que podem levar a doenças crônicas e ajudam a fortalecer o sistema imunológico. Possui baixas calorias, sem gordura, sem colesterol, sem glúten e com baixo teor de sódio. O consumo contínuo de Shimeji parece diminuir o risco de obesidade, diabetes, câncer, doenças cardíacas e fortalecer o sistema imune do corpo.',
+      referência: 'https://mushroomreferences.com'
     },
     {
       name: 'Pleurotus Ostreatus',
       comName:'Shimeji premium',
       image: shimofuriEspecie,
-      estacao: ['verao','inverno']
+      estacao: ['verao','inverno'],
+      descricao:'O Shimeji branco (Pleurotus ostreatus ) é uma fonte rica em proteínas, vitaminas, minerais, fibras e outros antioxidantes como o selênio, protegendo as células do corpo de danos que podem levar a doenças crônicas e ajudam a fortalecer o sistema imunológico. Possui baixas calorias, sem gordura, sem colesterol, sem glúten e com baixo teor de sódio. O consumo contínuo de Shimeji parece diminuir o risco de obesidade, diabetes, câncer, doenças cardíacas e fortalecer o sistema imune do corpo.',
+      referência: 'https://mushroomreferences.com'
     },
     ]
 
