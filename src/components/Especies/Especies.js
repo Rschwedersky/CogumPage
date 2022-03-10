31e428149534394ddb60d7591098782038ee204b
Rodrigo Porto Schwedersky <rodrigo_schwedersky@estudante.sesisenai.org.br>
2022-03-09 23:03:53 -0300

    NETLIFY

--- src/components/Especies/Especies.js
+++ src/components/Especies/Especies.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { CardGrid, Container} from './Especies.styles';
 import {useCogums} from './../../context/cogums/useCogums'
-import { GiSuperMushroom } from 'react-icons/gi';
+import { GiSuperMushroom, GiGrassMushroom } from 'react-icons/gi';
 import { CardEspecies } from '../CardEspecie/CardEspecie';
 export const Especies = () => {
     
     const {listaEspecie} = useCogums();
     return(<>
     <Container>
-        <h2 style={{marginLeft: '60px', color:'brown',fontFamily: 'Fredoka',textShadow:' 3px 2px 2px rgba(151, 150, 150, 1)'}}><GiSuperMushroom color='#2b2b2b'/> Nossos Cogums <GiSuperMushroom color='#2b2b2b'/></h2>
+        <h2 style={{marginLeft: '60px', color:'brown',fontFamily: 'Fredoka',textShadow:' 3px 2px 2px rgba(151, 150, 150, 1)'}}><GiGrassMushroom color='#2b2b2b'/><GiSuperMushroom color='#2b2b2b'/> Nossos Cogums <GiSuperMushroom color='#2b2b2b'/><GiGrassMushroom color='#2b2b2b'/></h2>
     <CardGrid>
     {listaEspecie.length === 0
     ? <></>
