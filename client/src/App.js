4d79bcef85b8ccb6d281dbe5df5ddc9fefb1ab27
Rschwedersky <cambara.ro@gmail.com>
2023-11-19 14:03:01 -0300

    server created

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+import routes from "./routes"
+import Layout from "./components/Layout"
+import Page404 from "./Pages/Page404"
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      element: <Layout />,
+      errorElement: <Page404 />,
+      children: routes
+    },
+  ])
+
+  return (
+      <RouterProvider router={router} />
+  )
+}
+
+export default App
